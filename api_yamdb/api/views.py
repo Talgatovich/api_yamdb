0de955c146a9772dc8a0dc52837a281d57875d56
Андрей Сафронов <c-r-o-n.mail.ru>
2022-03-17 19:59:23 +0300

    исправлено

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,41 @@
-<<<<<<< HEAD
-
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-
+from rest_framework import filters, permissions, viewsets
+from rest_framework.pagination import LimitOffsetPagination
+from titles.models import Category, Genre, Title
 from reviews.models import Review
 from .permissions import (AdminModeratorAuthorPermission,)
-from .serializers import (CommentSerializer, ReviewSerializer,)
+#from .permissions import 
+from .serializers import (CategorySerializer, GenreSerializer, CommentSerializer, ReviewSerializer,
+                          TitleReadSerializer, TitleWhiteSerializer)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    queryset = Title.objects.all()
+    #permission_classes = админ на запись, остальные чтение
+    pagination_class = LimitOffsetPagination
+    
+    def get_serializer_class(self):
+        # Если запрошенное действие (action) — получение списка объектов ('list')
+        # 'retrieve' - получение одного объекта
+        if self.action == 'list' or self.action == 'retrieve':
+            # ...то применяем TitleReadSerializer
+            return TitleReadSerializer
+        # А если запрошенное действие — не 'list', 'retrieve'
+        # применяем TitleWhiteSerializer
+        return TitleWhiteSerializer
+
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    #permission_classes = админ на запись, остальные чтение
+
+
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    #permission_classes = админ на запись, остальные чтение
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
@@ -39,40 +69,3 @@ class ReviewViewSet(viewsets.ModelViewSet):
             Title,
             id=self.kwargs.get('title_id'))
         serializer.save(author=self.request.user, title=title)
-=======
-from rest_framework import filters, permissions, viewsets
-from rest_framework.pagination import LimitOffsetPagination
-from titles.models import Category, Genre, Title
-
-#from .permissions import 
-from .serializers import (CategorySerializer, GenreSerializer,
-                          TitleReadSerializer, TitleWhiteSerializer)
-
-
-class TitleViewSet(viewsets.ModelViewSet):
-    queryset = Title.objects.all()
-    #permission_classes = админ на запись, остальные чтение
-    pagination_class = LimitOffsetPagination
-    
-    def get_serializer_class(self):
-        # Если запрошенное действие (action) — получение списка объектов ('list')
-        # 'retrieve' - получение одного объекта
-        if self.action == 'list' or self.action == 'retrieve':
-            # ...то применяем TitleReadSerializer
-            return TitleReadSerializer
-        # А если запрошенное действие — не 'list', 'retrieve'
-        # применяем TitleWhiteSerializer
-        return TitleWhiteSerializer
-
-
-class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Category.objects.all()
-    serializer_class = CategorySerializer
-    #permission_classes = админ на запись, остальные чтение
-
-
-class GenreViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Genre.objects.all()
-    serializer_class = GenreSerializer
-    #permission_classes = админ на запись, остальные чтение
->>>>>>> 11b2a857f06141ac901b9a929bbaa8ffb0107c7b
