f76b5ee68d74f86819aac55b9bed5aa6df75ffa2
Андрей Сафронов <c-r-o-n.mail.ru>
2022-03-24 18:23:46 +0300

    Исправления после ревью

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
+
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
